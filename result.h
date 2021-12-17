45bddda4b3dd94d9510788831b3356df1f8eb760
Neøw <71312500+Tommyriquet@users.noreply.github.com>
2021-12-17 18:03:32 +0100

    Create result.h

---
+++ result.h
@@ -0,0 +1,11 @@
+//
+// Created by tommy on 17/12/2021.
+//
+
+#ifndef PROJETOS_RESULT_H
+#define PROJETOS_RESULT_H
+
+void afficheResult(Voiture *v);
+int tri(const void *a, const void *b);
+
+#endif //PROJETOS_RESULT_H
