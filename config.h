6e3b09c76bc6cf256b0f7baa1cf3bcd6e5ee143b
Neøw <71312500+Tommyriquet@users.noreply.github.com>
2021-12-17 18:03:00 +0100

    Create config.h

---
+++ config.h
@@ -0,0 +1,19 @@
+//
+// Created by tommy on 17/12/2021.
+//
+
+#ifndef PROJET_CONFIG_H
+#define PROJET_CONFIG_H
+#define CHRONO_P1 5400
+#define CHRONO_P2 5400
+#define CHRONO_P3 3600
+#define CHRONO_Q1 1080
+#define CHRONO_Q2 900
+#define CHRONO_Q3 720
+#define NBRTOTALVOITURE 20 // Nombre de total voitures
+#define MAXTIMECOURSE 40000
+#define MINTIMECOURSE 25000
+#define DELAY 1
+
+
+#endif //PROJET_CONFIG_H
