0f73a886c0984406971e5ae28ebc71a719c6d3e7
Neøw <71312500+Tommyriquet@users.noreply.github.com>
2021-12-17 18:04:11 +0100

    Create course.h

---
+++ course.h
@@ -0,0 +1,12 @@
+//
+// Created by tommy on 17/12/2021.
+//
+
+#ifndef PROJET_COURSE_H
+#define PROJET_COURSE_H
+
+#include "voiture.h"
+
+void course(Voiture *v,int numVoiture,int nbrTour);
+
+#endif //PROJET_COURSE_H
