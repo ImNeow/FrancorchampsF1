9b736e3af5ab85e9fe895e42da6ca60ae1c83c2d
Neøw <71312500+Tommyriquet@users.noreply.github.com>
2021-12-18 13:21:16 +0100

    Update time.h

--- time.h
+++ time.h
@@ -5,6 +5,10 @@
 #ifndef PROJETOS_TIME_H
 #define PROJETOS_TIME_H
 
-int genererTemps();
+int genererTemps(int nbrTour);
+int crachTest();
+int getRandomSecteur();
+int stand();
+int randomStandTime();
 
 #endif //PROJETOS_TIME_H
