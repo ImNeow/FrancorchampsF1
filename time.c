30ecea5a5399fa2514cb68b7e463eb03f3c03078
Neøw <71312500+Tommyriquet@users.noreply.github.com>
2021-12-18 13:21:33 +0100

    Update time.c

--- time.c
+++ time.c
@@ -5,10 +5,30 @@
 #include <stdlib.h>
 #include "config.h"
 
-int genererTemps(){
+int genererTemps(int nbrTour){
 
     int randomTime= rand() % (MAXTIMECOURSE + 1 - MINTIMECOURSE) + MINTIMECOURSE;
 
     return randomTime;
 
+}
+
+int crachTest(){
+    if(rand()% PROBACRACH == 1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int getRandomSecteur(){
+    return rand()%3+1;
+}
+
+int stand(){
+    return rand() % PROBASTAND+1;
+}
+
+int randomStandTime(){
+    return rand() % (MAXSTANDTIME + 1 - MINSTANDTIME) + MINSTANDTIME;
 }
