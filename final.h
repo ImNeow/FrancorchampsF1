38b1a2e5ae93f2b02f68dfeca12c78e48cc2adfb
DziemiankoSebastian <64025269+DziemiankoSebastian@users.noreply.github.com>
2021-12-28 21:16:47 +0100

    Update final.h

--- final.h
+++ final.h
@@ -1,1 +1,14 @@
 
+//
+// Created by dziemiankosebastian on 28/12/21.
+//
+
+#ifndef PROJETOS_FINAL_H
+#define PROJETOS_FINAL_H
+
+#include "voiture.h"
+
+void final(Voiture *v,int numVoiture, int finalTours);
+
+
+#endif //PROJETOS_FINAL_H
