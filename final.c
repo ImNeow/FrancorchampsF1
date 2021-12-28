3b6e01cfe6b36fd52173eef952e0d5590ac5aee3
DziemiankoSebastian <64025269+DziemiankoSebastian@users.noreply.github.com>
2021-12-28 21:15:19 +0100

    Update final.c

--- final.c
+++ final.c
@@ -1,1 +1,15 @@
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include "voiture.h"
+#include "config.h"
+#include "time.h"
+
+void final(Voiture *v,int numVoiture, int finalTours) {
+
+    while (v->tour < finalTours) {
+
+    }
+
+}
